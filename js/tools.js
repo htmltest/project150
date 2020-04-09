39b413564b77996d20b868e221cfceaee570b086
htmltest <struev.vs@gmail.com>
2020-04-09 11:35:09 +0300

    first html

---
+++ js/tools.js
@@ -0,0 +1,335 @@
+$(document).ready(function() {
+
+    $.validator.addMethod('phoneRU',
+        function(phone_number, element) {
+            return this.optional(element) || phone_number.match(/^\+7 \d{3} \d{3}\-\d{2}\-\d{2}$/);
+        },
+        'Ошибка заполнения'
+    );
+
+    $('body').on('focus', '.form-input input, .form-input textarea', function() {
+        $(this).parent().addClass('focus');
+    });
+
+    $('body').on('blur', '.form-input input, .form-input textarea', function() {
+        $(this).parent().removeClass('focus');
+        if ($(this).val() != '') {
+            $(this).parent().addClass('full');
+        } else {
+            $(this).parent().removeClass('full');
+        }
+    });
+
+    $('body').on('input', '.form-input textarea', function() {
+        this.style.height = '242px';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+
+    $('body').on('change', '.form-file input', function() {
+        var curInput = $(this);
+        var curField = curInput.parents().filter('.form-file');
+        var curName = curInput.val().replace(/.*(\/|\\)/, '');
+        if (curName != '') {
+            curField.find('.form-file-name').html(curName);
+        } else {
+            curField.find('.form-file-name').html('');
+        }
+    });
+
+    $.validator.addMethod('inputDate',
+        function(curDate, element) {
+            if (this.optional(element) && curDate == '') {
+                return true;
+            } else {
+                if (curDate.match(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/)) {
+                    var userDate = new Date(curDate.substr(6, 4), Number(curDate.substr(3, 2)) - 1, Number(curDate.substr(0, 2)));
+                    if ($(element).attr('min')) {
+                        var minDateStr = $(element).attr('min');
+                        var minDate = new Date(minDateStr.substr(6, 4), Number(minDateStr.substr(3, 2)) - 1, Number(minDateStr.substr(0, 2)));
+                        if (userDate < minDate) {
+                            $.validator.messages['inputDate'] = 'Минимальная дата - ' + minDateStr;
+                            return false;
+                        }
+                    }
+                    if ($(element).attr('max')) {
+                        var maxDateStr = $(element).attr('max');
+                        var maxDate = new Date(maxDateStr.substr(6, 4), Number(maxDateStr.substr(3, 2)) - 1, Number(maxDateStr.substr(0, 2)));
+                        if (userDate > maxDate) {
+                            $.validator.messages['inputDate'] = 'Максимальная дата - ' + maxDateStr;
+                            return false;
+                        }
+                    }
+                    return true;
+                } else {
+                    $.validator.messages['inputDate'] = 'Дата введена некорректно';
+                    return false;
+                }
+            }
+        },
+        ''
+    );
+
+    $('form').each(function() {
+        initForm($(this));
+    });
+
+    $('.header-lang-link').click(function(e) {
+        $('.header-lang').toggleClass('open');
+        e.preventDefault();
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.header-lang').length == 0) {
+            $('.header-lang').removeClass('open');
+        }
+    });
+
+    $('.tabs').each(function() {
+        var curTabs = $(this);
+        var curTabsMenu = curTabs.find('> .tabs-menu');
+        var curTabsContainer = curTabs.find('> .tabs-container');
+        var newHTML = '';
+        curTabsContainer.find('> .tabs-content').each(function() {
+            var curTabTitle = $(this).find('> .tabs-content-title').html();
+            newHTML += '<div class="tabs-menu-item"><a href="#">' + curTabTitle + '</a></div> ';
+        });
+        curTabsContainer.find('> .tabs-content').eq(0).addClass('active');
+        curTabsMenu.html(newHTML);
+        curTabsMenu.find('.tabs-menu-item').eq(0).addClass('active');
+        curTabsMenu.mCustomScrollbar({
+            axis: 'x',
+            scrollButtons: {
+                enable: true
+            },
+            callbacks: {
+                onInit: function() {
+                    curTabsMenu.find('.mCSB_scrollTools.mCSB_scrollTools_horizontal .mCSB_buttonLeft').addClass('disabled');
+                },
+
+                whileScrolling: function() {
+                    if (this.mcs.leftPct == 100) {
+                        curTabsMenu.find('.mCSB_scrollTools.mCSB_scrollTools_horizontal .mCSB_buttonRight').addClass('disabled');
+                    } else {
+                        curTabsMenu.find('.mCSB_scrollTools.mCSB_scrollTools_horizontal .mCSB_buttonRight').removeClass('disabled');
+
+                    }
+
+                    if (this.mcs.leftPct == 0) {
+                        curTabsMenu.find('.mCSB_scrollTools.mCSB_scrollTools_horizontal .mCSB_buttonLeft').addClass('disabled');
+                    } else {
+                        curTabsMenu.find('.mCSB_scrollTools.mCSB_scrollTools_horizontal .mCSB_buttonLeft').removeClass('disabled');
+
+                    }
+                }
+            }
+        });
+    });
+
+    $('body').on('click', '.tabs-menu-item a', function(e) {
+        var curLi = $(this).parent();
+        if (!curLi.hasClass('active')) {
+            var curTabs = curLi.parents().filter('.tabs');
+            curTabs.find('.tabs-menu-item.active').removeClass('active');
+            curLi.addClass('active');
+            var curIndex = curTabs.find('.tabs-menu-item').index(curLi);
+            curTabs.find('.tabs-content.active').removeClass('active');
+            curTabs.find('.tabs-content').eq(curIndex).addClass('active');
+        }
+        e.preventDefault();
+    });
+
+    $('.header-menu').each(function() {
+        $('.header-menu ul').append('<li class="header-menu-hover"></li>');
+        var $navActive = $('.header-menu .header-menu-hover');
+
+        $navActive.data('origLeft', 0).data('origWidth', 0);
+
+        $('.header-menu ul li a').on('mouseover', function() {
+            var $el = $(this);
+            var leftPos = $el.parent().offset().left - $el.parent().parent().offset().left;
+            var newWidth = $el.parent().outerWidth();
+            $navActive.stop().animate({
+                left: leftPos,
+                width: newWidth
+            });
+        });
+
+        $('.header-menu ul li a').on('mouseout', function() {
+            $navActive.stop().animate({
+                left: $navActive.data('origLeft'),
+                width: $navActive.data('origWidth')
+            });
+        });
+    });
+
+    $('.dashboard-zone-dates-item a').click(function(e) {
+        var curLi = $(this).parent();
+        if (!curLi.hasClass('active')) {
+            $('.dashboard-zone-dates-item.active').removeClass('active');
+            curLi.addClass('active');
+            var curIndex = $('.dashboard-zone-dates-item').index(curLi);
+            $('.dashboard-zone-content.active').removeClass('active');
+            $('.dashboard-zone-content').eq(curIndex).addClass('active');
+        }
+        e.preventDefault();
+    });
+
+    $('body').on('click', '.support-search-link', function(e) {
+        $('.support-search').addClass('open');
+        $('.support-search-window-input input').trigger('focus');
+        e.preventDefault();
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.support-search').length == 0) {
+            $('.support-search').removeClass('open');
+        }
+    });
+
+});
+
+function initForm(curForm) {
+    curForm.find('.form-input input, .form-input textarea').each(function() {
+        if ($(this).val() != '') {
+            $(this).parent().addClass('full');
+        }
+    });
+
+    curForm.find('input.phoneRU').mask('+7 000 000-00-00');
+
+    curForm.find('.form-input textarea').each(function() {
+        $(this).css({'height': this.scrollHeight, 'overflow-y': 'hidden'});
+    });
+
+    curForm.find('.form-input-date input').mask('00.00.0000');
+    curForm.find('.form-input-date input').attr('autocomplete', 'off');
+    curForm.find('.form-input-date input').addClass('inputDate');
+
+    curForm.find('.form-input-date input').on('keyup', function() {
+        var curValue = $(this).val();
+        if (curValue.match(/^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/)) {
+            var isCorrectDate = true;
+            var userDate = new Date(curValue.substr(6, 4), Number(curValue.substr(3, 2)) - 1, Number(curValue.substr(0, 2)));
+            if ($(this).attr('min')) {
+                var minDateStr = $(this).attr('min');
+                var minDate = new Date(minDateStr.substr(6, 4), Number(minDateStr.substr(3, 2)) - 1, Number(minDateStr.substr(0, 2)));
+                if (userDate < minDate) {
+                    isCorrectDate = false;
+                }
+            }
+            if ($(this).attr('max')) {
+                var maxDateStr = $(this).attr('max');
+                var maxDate = new Date(maxDateStr.substr(6, 4), Number(maxDateStr.substr(3, 2)) - 1, Number(maxDateStr.substr(0, 2)));
+                if (userDate > maxDate) {
+                    isCorrectDate = false;
+                }
+            }
+            if (isCorrectDate) {
+                var myDatepicker = $(this).data('datepicker');
+                if (myDatepicker) {
+                    var curValueArray = curValue.split('.');
+                    myDatepicker.selectDate(new Date(Number(curValueArray[2]), Number(curValueArray[1]) - 1, Number(curValueArray[0])));
+                    myDatepicker.show();
+                    $(this).focus();
+                }
+            } else {
+                $(this).addClass('error');
+                return false;
+            }
+        }
+    });
+
+    curForm.find('.form-input-date input').each(function() {
+        var minDateText = $(this).attr('min');
+        var minDate = null;
+        if (typeof (minDateText) != 'undefined') {
+            var minDateArray = minDateText.split('.');
+            minDate = new Date(Number(minDateArray[2]), Number(minDateArray[1]) - 1, Number(minDateArray[0]));
+        }
+        var maxDateText = $(this).attr('max');
+        var maxDate = null;
+        if (typeof (maxDateText) != 'undefined') {
+            var maxDateArray = maxDateText.split('.');
+            maxDate = new Date(Number(maxDateArray[2]), Number(maxDateArray[1]) - 1, Number(maxDateArray[0]));
+        }
+        if ($(this).hasClass('maxDate1Year')) {
+            var curDate = new Date();
+            curDate.setFullYear(curDate.getFullYear() + 1);
+            curDate.setDate(curDate.getDate() - 1);
+            maxDate = curDate;
+            var maxDay = curDate.getDate();
+            if (maxDay < 10) {
+                maxDay = '0' + maxDay
+            }
+            var maxMonth = curDate.getMonth() + 1;
+            if (maxMonth < 10) {
+                maxMonth = '0' + maxMonth
+            }
+            $(this).attr('max', maxDay + '.' + maxMonth + '.' + curDate.getFullYear());
+        }
+        var startDate = new Date();
+        if (typeof ($(this).attr('value')) != 'undefined') {
+            var curValue = $(this).val();
+            if (curValue != '') {
+                var startDateArray = curValue.split('.');
+                startDate = new Date(Number(startDateArray[2]), Number(startDateArray[1]) - 1 , Number(startDateArray[0]));
+            }
+        }
+        $(this).datepicker({
+            language: 'ru',
+            minDate: minDate,
+            maxDate: maxDate,
+            startDate: startDate,
+            toggleSelected: false
+        });
+        if (typeof ($(this).attr('value')) != 'undefined') {
+            var curValue = $(this).val();
+            if (curValue != '') {
+                var startDateArray = curValue.split('.');
+                startDate = new Date(Number(startDateArray[2]), Number(startDateArray[1]) - 1 , Number(startDateArray[0]));
+                $(this).data('datepicker').selectDate(startDate);
+            }
+        }
+    });
+
+    curForm.find('.form-select select').each(function() {
+        var curSelect = $(this);
+        var options = {
+            minimumResultsForSearch: 99
+        }
+
+        curSelect.select2(options);
+        if (curSelect.find('option:selected').legnth > 0 || curSelect.find('option').legnth == 1 || curSelect.find('option:first').html() != '') {
+            curSelect.trigger({type: 'select2:select'})
+        }
+    });
+
+    curForm.validate({
+        ignore: ''
+    });
+}
+
+$(window).on('load resize scroll', function() {
+    var windowScroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+
+    if (windowScroll > 0) {
+        $('html').addClass('header-fixed');
+        var lastScroll = $('header').data('lastScroll');
+        if (typeof (lastScroll) == 'undefined') {
+            lastScroll = 0;
+        }
+
+        if (Math.abs(lastScroll - windowScroll) > 5) {
+            if (windowScroll > lastScroll){
+                $('header').addClass('header-up');
+            } else {
+                $('header').removeClass('header-up');
+            }
+            $('header').data('lastScroll', windowScroll);
+        }
+    } else {
+        $('html').removeClass('header-fixed');
+    }
+
+});
